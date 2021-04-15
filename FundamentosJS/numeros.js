29917060c1f6296c75080dba077234b58113276f
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-04-15 18:01:18 -0300

    Atualização.

---
+++ FundamentosJS/numeros.js
@@ -0,0 +1,25 @@
+const peso1 = 1
+const peso2 = Number ('2.0')
+
+console.log (peso1, peso2)
+console.log (Number.isInteger (peso1))
+console.log (Number.isInteger (peso2))
+
+const avaliacao1 = 9.5
+const avaliacao2 = 6.8
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total/ (peso1 + peso2)
+
+console.log (media)
+console.log (media.toFixed(2))
+console.log (media.toString(2)) //em binário
+console.log (typeof media)
+console.log (typeof Number) 
+
+//outros exemplos abaixo 
+
+console.log (7/0)
+console.log ("10" / 2)
+console.log ("Show!" * 2)
+console.log (0.1 + 0.7)
