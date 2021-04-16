4d2dd9874d1a1ad028cc8701111272a2cafd60d3
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-04-16 02:53:43 -0300

    atualizacao

--- FundamentosJS/math.js
+++ FundamentosJS/math.js
@@ -3,3 +3,7 @@ const area = Math.PI * Math.pow(raio, 2)
 
 console.log (area)
 console.log (typeof Math)
+
+console.log (3 + 3)
+console.log ('3' + 3)
+//sinal de + com string entre aspas faz concatenação ao invés de somar. 
---
+++ FundamentosJS/strin.js
@@ -0,0 +1,27 @@
+const escola = "cod3r"
+
+console.log(escola.charAt (4))
+console.log(escola.charAt (5))
+console.log(escola.charCodeAt (3))
+console.log(escola.indexOf ('3'))
+
+console.log(escola.substring (1))
+console.log(escola.substring(0, 3))
+
+console.log('escola '.concat(escola).concat("!"))
+//ou
+console.log('escola ' + escola + "!")
+
+console.log(escola.replace(3, 'e'))
+//ou
+console.log(escola.replace(/\d/, 'e'))  
+
+console.log ('ana,maria,pedro'.split(','))
+//o código acima gera uma array (vetor)
+
+
+
+
+
+
+
