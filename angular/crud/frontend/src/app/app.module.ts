6a8cdcc524b3d69d4e8379ab2d18078541fbe022
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-15 17:09:02 -0300

    Implementação angular read

--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,15 +10,32 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatCardModule } from  '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { BlueDirective } from './directives/blue.directive';
-import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatSnackBarModule } from  '@angular/material/snack-bar';
+import { HttpClientModule } from  '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { BlueDirective } from './directives/blue.directive';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from  '@angular/common';
+import { ForDirective } from './directives/for.directive';
+
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -28,9 +45,9 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    BlueDirective,
+    ProductCreateComponent,
+    ProductReadComponent,
     ForDirective,
-    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -39,9 +56,21 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
